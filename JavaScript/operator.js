8e8864685fe9d5828bccd0d21e436bccb4ca9455
Chetan Chauhan <devilmafia71@gmail.com>
2026-03-08 12:22:01 +0530

    operator file

---
+++ JavaScript/operator.js
@@ -0,0 +1,62 @@
+// operator vo keywords jo kisi operation krne ke kaaam aate h 
+
+// Aritmetic Operator , Logical Operator, Comparision Operator , Assignment Opertator
+
+// lets study all one by one...
+
+// Arithmetic operators (+, -, *, /, %) ....
+
+/*
+
+let x= 5;
+let y =6; 
+let result = x + y 
+
+console.log(result)
+ 
+let result1 = x - y 
+console.log(result1)
+
+
+let result2 = x / y 
+let result3 = 7 // 3  -> Aesi koi chij nhi hoti js mei // toh comment ke liye hi use hota h 
+let result4 = 7 % 3 
+let result5 = 2 ** 3 
+
+console.log(result2)
+console.log(result3)
+console.log(result4)
+console.log(result5)
+
+*/
+
+
+// Comparison operators (==, ===, !=, >, <)
+
+let a = 5;
+let b = "5"
+let c = 5
+let d = 6
+
+
+console.log(a == a)
+console.log(a === a)
+console.log(a == b)
+console.log(a === b)
+console.log(a == c)
+console.log(a === c)
+console.log(a == d)
+console.log(a === d)
+
+// so we can say that == is loosely comarision operator ,
+//  it even convert the datatype to make the variable same,
+// but  === is strictly comparion opetator, it compare then as it is only 
+
+
+/* 
+
+similary  !=, >, < operator are same 
+
+
+*/
+
